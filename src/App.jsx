be3461a187a3528d39df5691f54747f5d437a886
MK <marmorakh2000@gmail.com>
2025-07-27 14:21:23 +0300

    everything going ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
-
 function App() {
   return (
-    <div className=''>App</div>
+    
+      <body className='flex-row'>
+        <div className='flex-col m-0 px-[min(2.5vw,20px)] pt-15 bg-amber-50  h-full round fixed text-center'><h1>a little bit <br></br>about myself</h1><br></br>
+          <div className='text-left'>courses I've completed <ul> 
+              <li><img src='src/pho.jpg' className='icon'/><p>AWS</p>  </li> <li></li> <li></li> <li></li> </ul></div><hl></hl></div>
+        <div></div>
+      </body>
+
   )
 }
 
