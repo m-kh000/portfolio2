e8ebe7291ba7a3a951298e1da342195c1eacecad
MK <marmorakh2000@gmail.com>
2025-07-27 21:39:42 +0300

    added photos and fixing scale

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ function App() {
   return (
     
       <body className='flex-row'>
-        <div className='flex-col m-0 px-[min(2.5vw,20px)] pt-15 bg-amber-50  h-full round fixed text-center'><h1>a little bit <br></br>about myself</h1><br></br>
-          <div className='text-left'>courses I've completed <ul> 
-              <li><img src='src/pho.jpg' className='icon'/><p>AWS</p>  </li> <li></li> <li></li> <li></li> </ul></div><hl></hl></div>
+        <div className='flex-col max-w-[min(37vw,260px)] m-0 px-2 pt-15 bg-amber-50  h-full round text-center'><h1>a little bit <br></br>about myself</h1><br></br>
+          <div className='text-left'><p>courses I've completed </p><br/><ul> 
+              <li><img src='src/ai.png' className='icon'/><p>AWS Educate machine learning foundations</p> </li> <li><img src='src/ai.png' className='icon'/><p>Introducing Generative AI with AWS</p></li> <li><img src='src/ai.png' className='icon'/><p>Object Oriented Programming in Java Specialization Duke University</p></li> <li><img src='src/ai.png' className='icon'/><p>CS50 Harvard</p></li> <li><img src='src/paint.png' className='icon'/><p>Fundamentals of Graphic Design
+California Institute of the Arts</p></li> </ul></div></div>
         <div></div>
       </body>
 
