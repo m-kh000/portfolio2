3ffb92d4792be4c5501926215ae0a6d25609bb79
MK <marmorakh2000@gmail.com>
2025-07-27 22:40:53 +0300

    NOT crazy

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,63 @@
-import React from 'react'
+import React from "react";
 function App() {
   return (
-    
-      <body className='flex-row'>
-        <div className='flex-col max-w-[min(37vw,260px)] m-0 px-2 pt-15 bg-amber-50  h-full round text-center'><h1>a little bit <br></br>about myself</h1><br></br>
-          <div className='text-left'><p>courses I've completed </p><br/><ul> 
-              <li><img src='src/ai.png' className='icon'/><p>AWS Educate<line></line>  machinelearning foundations</p> </li> <li><img src='src/ai.png' className='icon'/><p>Introducing Generative AI with AWS</p></li> <li><img src='src/ai.png' className='icon'/><p>Object Oriented Programming in Java Specialization Duke University</p></li> <li><img src='src/ai.png' className='icon'/><p>CS50 Harvard</p></li> <li><img src='src/paint.png' className='icon'/><p>Fundamentals of Graphic Design
-California Institute of the Arts</p></li> </ul></div></div>
-        <div></div>
-      </body>
-
-  )
+    <div  className="flex-row ">
+      <div className="flex-col max-w-[min(40vw,300px)] m-0 px-2 pt-10 bg-amber-50  h-full round text-center">
+        <button
+          className="rounded-4xl float"
+          //onClick={() =>}
+        >
+          <img id="sun" src="src/sun.png" className="icon"></img>
+        </button>
+        <br /><br />
+        <h1>
+          a little bit <br></br>about myself
+        </h1>
+        <br />
+        <div className="text-left">
+          <p>courses I've completed </p>
+          <br />
+          <ul>
+            <li>
+              <img src="src/ai.png" className="icon" />
+              <p>
+                AWS Educate machine
+                learning foundations
+              </p>{" "}
+            </li>{" "}
+            <li>
+              <img src="src/ai.png" className="icon" />
+              <p>
+                Introducing Generative
+                AI with AWS
+              </p>
+            </li>{" "}
+            <li>
+              <img src="src/ai.png" className="icon" />
+              <p>
+                Object Oriented
+                Programming in Java
+                Specialization Duke
+                University
+              </p>
+            </li>{" "}
+            <li>
+              <img src="src/ai.png" className="icon" />
+              <p>CS50 Harvard</p>
+            </li>{" "}
+            <li>
+              <img src="src/paint.png" className="icon" />
+              <p>
+                Fundamentals of Graphic
+                Design California
+                Institute of the Arts
+              </p>
+            </li>{" "}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
