fd6cfd4b193b8a50ecf763cc3ffc3b5a59de6ad2
MK <marmorakh2000@gmail.com>
2025-07-28 21:10:33 +0300

    its good its stunning

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
   };
 
   return (
-    <div className="flex-row">
+    <div className="flex">
       <div id="toolbar"className="flex-col max-w-[min(40vw,300px)] m-0 px-2 pt-10  h-full round text-center">
         <button
           id="mode"
@@ -24,9 +24,9 @@ function App() {
           onClick={toggleMode}
         >
           {isDarkMode ? (
-            <img src="src/moon.png" alt="Moon" className="icon" />
+            <img src="/moon.png" alt="Moon" className="icon" />
           ) : (
-            <img src="src/sun.png" alt="Sun" className="icon" />
+            <img src="/sun.png" alt="Sun" className="icon" />
           )}
         </button>
 
@@ -43,28 +43,29 @@ function App() {
           <br />
           <ul>
             <li className="flex items-start gap-2">
-              <img src="src/ai.png" className="icon " />
+              <img src="/ai.png" className="icon " />
               <p>AWS Educate Machine Learning Foundations</p>
             </li>
             <li className="flex items-start gap-2">
-              <img src="src/ai.png" className="icon " />
+              <img src="/ai.png" className="icon " />
               <p>Introducing Generative AI with AWS</p>
             </li>
             <li className="flex items-start gap-2">
-              <img src="src/ai.png" className="icon " />
+              <img src="/ai.png" className="icon " />
               <p>Object Oriented Programming in Java - Duke University</p>
             </li>
             <li className="flex items-start gap-2">
-              <img src="src/ai.png" className="icon " />
+              <img src="/ai.png" className="icon " />
               <p>CS50 - Harvard</p>
             </li>
             <li className="flex items-start gap-2">
-              <img src="src/paint.png" className="icon " />
+              <img src="/paint.png" className="icon " />
               <p>Fundamentals of Graphic Design - CalArts</p>
             </li>
           </ul>
         </div>
       </div>
+      <img src="1.png" className="img"/>
     </div>
   );
 }
